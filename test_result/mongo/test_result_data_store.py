aa5b56d2512a20e9d6fb65dbf8651496b98e4564
GodQ <quchuanhao@gmail.com>
2022-03-12 18:19:24 +0800

    fix list testrun NPE

--- test_result/mongo/test_result_data_store.py
+++ test_result/mongo/test_result_data_store.py
@@ -96,7 +96,7 @@ class MongoTestResultDataStore(TestResultDataStoreInterface):
         return data
 
     def get_testrun_list_id_only(self, project_id, params=None):
-        if not params.get('limit'):
+        if not params or not params.get('limit'):
             params['limit'] = 999999
         params = reformat_params(params)
         offset = params.get("offset", 0)
